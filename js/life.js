7cb3c4a1fa574fb00822d36f5ca640c97b04ee85
Simon Zou <simon.zou@gmail.com>
2015-05-29 13:30:53 -0700

    added html form field, separated cards into functions, added card for nobel laureates

--- js/life.js
+++ js/life.js
@@ -2,23 +2,49 @@ $(document).ready(function(){
     url = "https://spreadsheets.google.com/feeds/list/1JFFjBvaO_PAs3d0jFWYmgXnh3A00MSeg3G4G1qYC46w/od6/public/values?alt=json"
 	$.getJSON(url, function(json){
 		var data = clean_google_sheet_json(json);
-
 		var events = _.filter(data, function(datum) {return datum.type === "event" && datum.category === "ucla-events"} );
-
-		console.log(events);
-		for (var i = 0; i < events.length; i++) {
-			var card_html = compile_template_to_html("#event-card-template", events[i]);
-			switch (i % 3) {
-				case 0: 		
-					$("#ucla-events-left").append(card_html);
-					break;
-				case 1: 
-					$("#ucla-events-mid").append(card_html);
-					break;
-				case 2:
-					$("#ucla-events-right").append(card_html);
-					break;
-			}
-		}
+		var nobels = _.filter(data, function(datum) {return datum.id === "nobel" });
+		// $("#submit-button").click(function(){
+		// 	clear_page();
+			add_ucla_event_cards(events);
+			add_nobel_card(nobels);
+		// })
 	});
 });
+
+
+function add_nobel_card(nobels) {
+	var data = {
+		'singlestat': nobels.length,
+		'pretext': 'UCLA Faculty and Alumni have won',
+		'posttext': nobels.length === 1 ? 'Nobel Prize' : 'Nobel Prizes'
+	}
+	var card_html = compile_template_to_html("#single-number-template", data);
+	console.log(card_html);
+	$("#ucla-events-left").append(card_html);
+
+}
+
+function add_ucla_event_cards(events) {
+	for (var i = 0; i < events.length; i++) {
+		var card_html = compile_template_to_html("#event-card-template", events[i]);
+		switch (i % 3) {
+			case 0: 		
+				$("#ucla-events-left").append(card_html);
+				break;
+			case 1: 
+				$("#ucla-events-mid").append(card_html);
+				break;
+			case 2:
+				$("#ucla-events-right").append(card_html);
+				break;
+		}
+	}
+}
+
+function clear_page() {
+	$("#ucla-events-left").html("");
+	$("#ucla-events-mid").html("");
+	$("#ucla-events-right").html("");
+
+}
