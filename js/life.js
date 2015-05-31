a8fe3d9d196a7922f9b6b10031a59fa008e26be5
Simon Zou <simon.zou@gmail.com>
2015-05-31 16:12:12 -0700

    cards fade in different order if a mobile browser is detected

--- js/life.js
+++ js/life.js
@@ -7,18 +7,23 @@ var selection_to_datestring = {
 	'fall2014': '9/1/2014',
 };
 
+card_ids = []
 
 $(document).ready(function(){
 
-	var selection = window.location.search;
-	if (selection) {
-		selection = selection.split('=')[1];
+	// if url has ?start=fall2009, selection will equal fall2009
+	var selection = window.location.search.split('=')[1];
+	$(".category-header").hide();
+	// check to see that we have a valid selection
+	if (selection in selection_to_datestring) {
+		$(".category-header").fadeIn(200);
+		
+		// fix selection in dropdown
 		document.getElementsByName('start')[0].value = selection;
 
 	    url = "https://spreadsheets.google.com/feeds/list/1JFFjBvaO_PAs3d0jFWYmgXnh3A00MSeg3G4G1qYC46w/od6/public/values?alt=json"
 		$.getJSON(url, function(json){
 
-			//var selection = "fall2009";
 			var data = clean_google_sheet_json(json);
 
 			// filters out data points that are older than selected date
@@ -27,20 +32,21 @@ $(document).ready(function(){
 				var now = new Date();
 				return timestamp >= Date.parse(selection_to_datestring[selection]) && timestamp <= Date.parse(now);
 			});
+
 			var events = _.filter(data, function(datum) {return datum.type === "event" && datum.category === "ucla-events"} );
 			var nobels = _.filter(data, function(datum) {return datum.id === "nobel"; });
 			var construction_events = _.filter(data, function(datum) {return datum.id === "construction"; });
-			// $("#submit-button").click(function(){
-			// 	clear_page();
+			
 			add_construction_card(construction_events);
-
 			if (nobels.length > 0)
 				add_nobel_card(nobels);
 			add_ucla_event_cards(events);
-			// })
-			//console.log(card_ids);
-			var card_ids = get_card_ids_by_column("ucla-events");
-			add_cards(card_ids);
+			
+			// if mobile, cards need to fade in a different order (vertical first)
+			// if on desktop, cards need to fade in horizontally
+			if (!window.mobilecheck())
+				card_ids = get_card_ids_by_column("ucla-events");
+			fade_in_cards(card_ids);
 		});
 	}
 });
@@ -100,7 +106,7 @@ function add_construction_card(events) {
 	//console.log(chart_labels);
 	//console.log(events_by_year);
 	$("#construction").hide();
-	//card_ids.push("#construction");
+	card_ids.push("#construction");
 }
 
 
@@ -115,14 +121,14 @@ function add_nobel_card(nobels) {
 	var card_html = compile_template_to_html("#single-number-template", data);
 	//console.log(card_html);
 	$("#ucla-events-left").append(card_html);
-	//card_ids.push("#nobel");
+	card_ids.push("#nobel");
 
 }
 
 function add_ucla_event_cards(events) {
 	for (var i = 0; i < events.length; i++) {
 		var card_html = compile_template_to_html("#event-card-template", events[i]);
-		//card_ids.push("#" + events[i].id);
+		card_ids.push("#" + events[i].id);
 		switch (i % 3) {
 			case 0: 		
 				$("#ucla-events-left").append(card_html);
@@ -165,18 +171,12 @@ function addCommas(nStr)
 	return x1 + x2;
 }
 
-function add_cards(card_ids) {
+function fade_in_cards(card_ids) {
 	console.log(card_ids);
 	$(card_ids.join(", ")).hide();
 	for (var i = 0; i < card_ids.length; i++) {
 		$(card_ids[i]).delay(i*500).fadeIn(500);
 	}
-	// for (var i = 0; i < card_ids.length; i++) {
-	// 	// setTimeout(function () {
-	// 		$(card_ids[i]).hide();
-	// 		$(card_ids[i]).fadeIn(2000);
-	// 	// }, 1000);
-	// }
 }
 
 function get_card_ids_by_column(main_div_id) {
@@ -200,4 +200,10 @@ function get_card_ids_by_column(main_div_id) {
 			card_ids.push("#" + $(right_div_id).children()[i].id);
 	}
 	return card_ids;
+}
+
+window.mobilecheck = function() {
+  var check = false;
+  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
+  return check;
 }
