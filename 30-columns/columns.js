8842501bbcd44e85acbd58601a943643a1382df4
vivz <vivian-keke@hotmail.com>
2015-06-03 13:53:36 -0700

    I started

---
+++ 30-columns/columns.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    
+    $('.article').hide();
+    
+    $(function(){
+        // Bind the event.
+        $(window).hashchange( function(){
+            // Alerts every time the hash changes!
+            console.log( location.hash );
+            
+            if (location.hash == "") {
+                $('.article').hide();
+                $('#descriptor').show();
+            } else {
+                $('#descriptor').hide();
+                $('.article').hide();
+                $(location.hash).show();
+            }
+        });
+
+        // Trigger the event (useful on page load).
+        $(window).hashchange();
+
+    });;
+})
+
+//FORCE CHANGE
