038d62987d76729a034c62ae14e69c54b0e83c28
vivz <vivian-keke@hotmail.com>
2015-06-04 02:29:28 -0700

    thumbnail name overlay

--- 30-columns/columns.js
+++ 30-columns/columns.js
@@ -1,13 +1,22 @@
 $(document).ready(function(){
 
-    
+//==========Add names to thumbnail overlay=======
+  $(".thumb").mouseover(function ()
+  {
+     var str=$(this).context.hash;
+     var name=str.substring(1);
+     var div =$(this).context.children[1];
+     if(div.innerHTML=="")
+        div.innerHTML+=name;
+  });
+
+//=========Change Hash================
     $('.article').hide();
-    
     $(function(){
         // Bind the event.
         $(window).hashchange( function(){
             // Alerts every time the hash changes!
-            console.log( location.hash );
+ 
             
             if (location.hash == "") {
                 $('.article').hide();
@@ -24,3 +33,4 @@ $(document).ready(function(){
 
     });;
 })
+
