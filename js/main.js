53e8457bcf88410b9e70e99b56a47967baa24f45
Chang Liu <shampliu@gmail.com>
2015-06-05 22:32:53 -0700

    dope shit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
 $(document).foundation();
 
 $(document).ready(function() {
-    $('#fullpage').fullpage();
+
+  $('#fullpage').fullpage({
+  	anchors: ['firstPage', 'secondPage', '3rdPage'],
+    // sectionsColor: ['#C63D0F', '#1BBC9B', '#7E8F7C'],
+    navigation: true,
+    navigationPosition: 'right',
+    navigationTooltips: ['First', 'Second', 'Third'],
+    onLeave: function(index, nextIndex, direction){
+
+      //after leaving section 2
+      if(index == 1 && direction =='down'){
+      	$('#banner > h1').velocity({ opacity: 0 }, { display: "none" });
+      }
+
+      if(index == 2 && direction =='up'){
+      	$('#banner > h1').velocity({ opacity: 1 }, { display: "block" });
+      }
+
+    }
+  });
 });
