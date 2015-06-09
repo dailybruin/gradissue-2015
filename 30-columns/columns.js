b699d8daa6ed4d0ca6f5560fae6cabd142dc34df
Simon Zou <simon.zou@gmail.com>
2015-06-08 17:33:25 -0700

    audio stops playing when you switch columns

--- 30-columns/columns.js
+++ 30-columns/columns.js
@@ -26,6 +26,7 @@ $(document).ready(function(){
                 $('.article').hide();
                   $('#descriptor').show();
                 $(location.hash).show();
+                $("audio")[0].pause();
             }
         });
 
